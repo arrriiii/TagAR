705cabc4462620e1055fed72ae8205b92d99e16f
Ari <dominiquearia0@gmail.com>
2018-07-13 11:24:35 -0700

    Added name for pointer (camera center) in view controller, added home storyboard for home menu

--- TaggAR/ViewController.swift
+++ TaggAR/ViewController.swift
@@ -13,14 +13,19 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     @IBOutlet weak var arView: ARSCNView!
     let configuration = ARWorldTrackingConfiguration()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.arView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
         self.arView.showsStatistics = true
         self.arView.session.run(configuration)
         self.arView.delegate = self
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
+    
+//    let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+//    arView.addGestureRecognizer(tapGestureRecognizer)
+// Do any additional setup after loading the view, typically from a nib.
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -31,22 +36,23 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         guard let pointView = arView.pointOfView else {return}
         let transform = pointView.transform
         let cameraOrigin = SCNVector3(-transform.m31,-transform.m32,-transform.m33)
-        let currentLocation = SCNVector3(transform.m41, transform.m42, transform.m43)
-        let currentPositionOfCamera = cameraOrigin + currentLocation
+        let translation = SCNVector3(transform.m41, transform.m42, transform.m43)
+        let cameraRelativePosition = cameraOrigin + translation
+       
         DispatchQueue.main.async {
             if self.draw.isHighlighted {
                 let sphereNode = SCNNode(geometry: SCNSphere(radius: 0.02))
-                sphereNode.position = currentPositionOfCamera
+                sphereNode.position = cameraRelativePosition
                 self.arView.scene.rootNode.addChildNode(sphereNode)
                 sphereNode.geometry?.firstMaterial?.diffuse.contents = UIColor.red
                 print("draw button is being pressed")
             } else {
-//                let pointer = SCNNode(geometry: SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.1/2))
                 let pointer = SCNNode(geometry: SCNPlane(width: 0.1, height: 0.1))
-                pointer.position = currentPositionOfCamera
+                pointer.name = "center"
+                pointer.position = cameraRelativePosition
 
                 self.arView.scene.rootNode.enumerateChildNodes({ (node, _) in
-                    if node.geometry is SCNPlane {
+                    if node.name == "center" {
                         node.removeFromParentNode()
                     }
                 })
@@ -56,10 +62,14 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             }
         }
     }
-//        currentLocation is location translationally
+//        translation is location translationally
 //        cameraOrigin is the initial orientation of the camera
 
 
+}
+
+func colorPicker() {
+    
 }
 
 func +(left: SCNVector3, right: SCNVector3) -> SCNVector3 {
