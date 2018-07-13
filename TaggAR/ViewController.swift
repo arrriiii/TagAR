c11bb020c870473b4ecb0c21c564f21a7742f075
Ari <dominiquearia0@gmail.com>
2018-07-12 17:43:55 -0700

    Initial Commit

---
+++ TaggAR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TaggAR
+//
+//  Created by Ari Herman on 7/12/18.
+//  Copyright © 2018 Ari Herman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
