55e9e7f05e7045cadb844754931a3584a9218922
Ari <dominiquearia0@gmail.com>
2018-07-12 19:45:54 -0700

    Added sphere and pointer to viewController

--- TaggAR/ViewController.swift
+++ TaggAR/ViewController.swift
@@ -9,8 +9,10 @@
 import UIKit
 import ARKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, ARSCNViewDelegate {
+    
+    @IBOutlet weak var draw: UIButton!
+    
     @IBOutlet weak var arView: ARSCNView!
     let configuration = ARWorldTrackingConfiguration()
     override func viewDidLoad() {
@@ -18,6 +20,7 @@ class ViewController: UIViewController {
         self.arView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
         self.arView.showsStatistics = true
         self.arView.session.run(configuration)
+        self.arView.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -25,7 +28,40 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func renderer(_ renderer: SCNSceneRenderer, willRenderScene scene: SCNScene, atTime time: TimeInterval) {
+        guard let pointView = arView.pointOfView else {return}
+        let transform = pointView.transform
+        let cameraOrigin = SCNVector3(-transform.m31,-transform.m32,-transform.m33)
+        let currentLocation = SCNVector3(transform.m41, transform.m42, transform.m43)
+        let currentPositionOfCamera = cameraOrigin + currentLocation
+        DispatchQueue.main.async {
+            if self.draw.isHighlighted {
+                let sphereNode = SCNNode(geometry: SCNSphere(radius: 0.02))
+                sphereNode.position = currentPositionOfCamera
+                self.arView.scene.rootNode.addChildNode(sphereNode)
+                sphereNode.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+                print("draw button is being pressed")
+            } else {
+                let pointer = SCNNode(geometry: SCNSphere(radius: 0.01))
+                pointer.position = currentPositionOfCamera
+
+                self.arView.scene.rootNode.enumerateChildNodes({ (node, _) in
+                    node.removeFromParentNode()
+                })
+                
+                self.arView.scene.rootNode.addChildNode(pointer)
+                pointer.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "Pointer")
+            }
+        }
+    }
+//        currentLocation is location translationally
+//        cameraOrigin is the initial orientation of the camera
 
 
+}
+
+func +(left: SCNVector3, right: SCNVector3) -> SCNVector3 {
+    return SCNVector3Make(left.x + right.x, left.y + right.y, left.z + right.z)
 }
 
