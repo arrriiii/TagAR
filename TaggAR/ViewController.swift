e7187a1c686f2f0263bf2f17cc22a51aa31db99b
Ari <dominiquearia0@gmail.com>
2018-07-18 14:12:28 -0700

    Added color picker buttons and reset button to viewController

--- TaggAR/ViewController.swift
+++ TaggAR/ViewController.swift
@@ -6,13 +6,24 @@
 
 import UIKit
 import ARKit
+import SceneKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
     
     @IBOutlet weak var draw: UIButton!
     
     @IBOutlet weak var arView: ARSCNView!
+    
+    @IBOutlet weak var hiddenButton: UIButton!
+    
+    //    IBOutlet-reference to storyboard/ IBAction- reference to an action
+    
     let configuration = ARWorldTrackingConfiguration()
+//    let for values that do not change/ var for values that do change
+    var colorPicker = UIColor.black
+    
+    var showHiddenButton = false
+    var canvasNode = SCNNode()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,12 +31,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         self.arView.showsStatistics = true
         self.arView.session.run(configuration)
         self.arView.delegate = self
-        
+        self.arView.isUserInteractionEnabled = true
+        arView.scene.rootNode.addChildNode(canvasNode)
     }
-    
-//    let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
-//    arView.addGestureRecognizer(tapGestureRecognizer)
-// Do any additional setup after loading the view, typically from a nib.
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -40,39 +48,78 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         let cameraRelativePosition = cameraOrigin + translation
        
         DispatchQueue.main.async {
+            
             if self.draw.isHighlighted {
                 let sphereNode = SCNNode(geometry: SCNSphere(radius: 0.02))
                 sphereNode.position = cameraRelativePosition
-                self.arView.scene.rootNode.addChildNode(sphereNode)
-                sphereNode.geometry?.firstMaterial?.diffuse.contents = UIColor.red
-                print("draw button is being pressed")
-            } else {
-                let pointer = SCNNode(geometry: SCNPlane(width: 0.1, height: 0.1))
-                pointer.name = "center"
-                pointer.position = cameraRelativePosition
-
-                self.arView.scene.rootNode.enumerateChildNodes({ (node, _) in
-                    if node.name == "center" {
-                        node.removeFromParentNode()
-                    }
-                })
+                sphereNode.geometry?.firstMaterial?.diffuse.contents = self.colorPicker
+               
+                if self.showHiddenButton == false {
+                    self.buttonIsVisible()
+                }
                 
-                self.arView.scene.rootNode.addChildNode(pointer)
-                pointer.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "Pointer")
+                self.canvasNode.addChildNode(sphereNode)
+//                self.arView.scene.rootNode.addChildNode(sphereNode)
+                print("draw button is being pressed")
             }
         }
     }
-//        translation is location translationally
-//        cameraOrigin is the initial orientation of the camera
-
-
-}
-
-func colorPicker() {
+    
+    func buttonIsVisible() {
+        self.showHiddenButton = true
+        hiddenButton.isHidden = false
+    }
+    
+    
+    @IBAction func reset(_ sender: Any) {
+        self.canvasNode.enumerateChildNodes { (node, _) in
+           node.removeFromParentNode()
+        }
+        hiddenButton.isHidden = true
+        self.showHiddenButton = false
+    }
+    
+    @IBAction func red(_ sender: Any) {
+        self.colorPicker = UIColor.red
+    }
+    
+    @IBAction func blue(_ sender: Any) {
+        self.colorPicker = UIColor.blue
+    }
+    
+    @IBAction func black(_ sender: Any) {
+        self.colorPicker = UIColor.black
+    }
+    
+    @IBAction func green(_ sender: Any) {
+        self.colorPicker = UIColor.green
+    }
+    
+    @IBAction func white(_ sender: Any) {
+        self.colorPicker = UIColor.white
+    }
     
 }
 
 func +(left: SCNVector3, right: SCNVector3) -> SCNVector3 {
     return SCNVector3Make(left.x + right.x, left.y + right.y, left.z + right.z)
 }
+
+//        translation is location translationally
+//        cameraOrigin is the initial orientation of the camera
+
+//            else {
+//
+//                let pointer = SCNNode(geometry: SCNBox(width: 0.01, height: 0.01, length: 0.01, chamferRadius: 0.01/2))
+//                pointer.name = "center"
+//                pointer.position = cameraRelativePosition
+//
+//                self.arView.scene.rootNode.enumerateChildNodes({ (node, _) in
+//                    if node.name == "center" {
+//                        node.removeFromParentNode()
+//                    }
+//                })
+//                self.arView.scene.rootNode.addChildNode(pointer)
+//                pointer.geometry?.firstMaterial?.diffuse.contents = String("X")
+//            }
 
