7999a3d0f4071db52460359789d52706595865c9
Ari <dominiquearia0@gmail.com>
2018-07-24 21:38:21 -0700

    updated menu button

--- TaggAR/ViewController.swift
+++ TaggAR/ViewController.swift
@@ -15,6 +15,25 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     @IBOutlet weak var arView: ARSCNView!
     @IBOutlet weak var hiddenButton: UIButton!
     
+    @IBOutlet weak var redColor: UIButton!
+    @IBOutlet weak var whiteColor: UIButton!
+    @IBOutlet weak var purpleColor: UIButton!
+    @IBOutlet weak var yellowColor: UIButton!
+    @IBOutlet weak var blackColor: UIButton!
+    @IBOutlet weak var greenColor: UIButton!
+    @IBOutlet weak var blueColor: UIButton!
+    @IBOutlet weak var more: UIButton!
+    
+    var redButtonLocation : CGPoint!
+    var whiteButtonLocation : CGPoint!
+    var purpleButtonLocation : CGPoint!
+    var yellowButtonLocation : CGPoint!
+    var blackButtonLocation : CGPoint!
+    var greenButtonLocation : CGPoint!
+    var blueButtonLocation : CGPoint!
+    
+    
+    
     // IBOutlet-reference to storyboard/ IBAction- reference to an action
     
     let configuration = ARWorldTrackingConfiguration()
@@ -29,6 +48,25 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         self.arView.delegate = self
         self.arView.isUserInteractionEnabled = true
         arView.scene.rootNode.addChildNode(canvasNode)
+        
+        
+        redButtonLocation = redColor.center
+        whiteButtonLocation = whiteColor.center
+        purpleButtonLocation = purpleColor.center
+        yellowButtonLocation = yellowColor.center
+        blackButtonLocation = blackColor.center
+        greenButtonLocation = greenColor.center
+        blueButtonLocation = blueColor.center
+        
+        
+        redColor.center = more.center
+        whiteColor.center = more.center
+        purpleColor.center = more.center
+        blackColor.center = more.center
+        yellowColor.center = more.center
+        greenColor.center = more.center
+        blueColor.center = more.center
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -60,6 +98,57 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         }
     }
     
+    func toggleButton(button: UIButton, onImage: UIImage, offImage: UIImage) {
+        if button.currentImage == offImage {
+            button.setImage(onImage, for: .normal)
+        } else {
+            button.setImage(offImage, for: .normal)
+        }
+    }
+    
+    @IBAction func moreClicked(_ sender: UIButton) {
+        if more.currentImage == #imageLiteral(resourceName: "more_off") {
+            UIView.animate(withDuration: 0.3, animations: {
+                
+                self.redColor.alpha = 1
+                self.whiteColor.alpha = 1
+                self.purpleColor.alpha = 1
+                self.blackColor.alpha = 1
+                self.yellowColor.alpha = 1
+                self.greenColor.alpha = 1
+                self.blueColor.alpha = 1
+                
+                self.redColor.center = self.redButtonLocation
+                self.whiteColor.center = self.whiteButtonLocation
+                self.purpleColor.center = self.purpleButtonLocation
+                self.blackColor.center = self.blackButtonLocation
+                self.yellowColor.center = self.yellowButtonLocation
+                self.greenColor.center = self.greenButtonLocation
+                self.blueColor.center = self.blueButtonLocation
+            })
+        } else {
+            UIView.animate(withDuration: 0.3, animations: {
+                self.redColor.alpha = 0
+                self.whiteColor.alpha = 0
+                self.purpleColor.alpha = 0
+                self.blackColor.alpha = 0
+                self.yellowColor.alpha = 0
+                self.greenColor.alpha = 0
+                self.blueColor.alpha = 0
+                
+                self.redColor.center = self.more.center
+                self.whiteColor.center = self.more.center
+                self.purpleColor.center = self.more.center
+                self.blackColor.center = self.more.center
+                self.yellowColor.center = self.more.center
+                self.greenColor.center = self.more.center
+                self.blueColor.center = self.more.center
+            })
+            
+        }
+        toggleButton(button: sender, onImage: #imageLiteral(resourceName: "more_on"), offImage: #imageLiteral(resourceName: "more_off"))
+    }
+    
     func buttonIsVisible() {
         self.showResetButton = true
         hiddenButton.isHidden = false
@@ -73,6 +162,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         hiddenButton.isHidden = true
         self.showResetButton = false
     }
+    
     
     @IBAction func red(_ sender: Any) {
         self.colorPicker = UIColor.red
@@ -92,6 +182,14 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     @IBAction func white(_ sender: Any) {
         self.colorPicker = UIColor.white
+    }
+    
+    @IBAction func purple(_ sender: Any) {
+        self.colorPicker = UIColor.purple
+    }
+    
+    @IBAction func yellow(_ sender: Any) {
+        self.colorPicker = UIColor.yellow
     }
     
 }
